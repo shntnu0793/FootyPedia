fc38a6cd8731d8e99a407e1284d45dbc3846642e
shntnu0793 <85308052+shntnu0793@users.noreply.github.com>
2021-06-03 22:47:32 +0530

    Add files via upload

---
+++ src/components/Home/home.js
@@ -0,0 +1,63 @@
+import './home.css'
+import Profile from '../Profile/profile'
+import React, { useState } from 'react'
+
+
+
+
+const Home = () => {
+   const [query, setQuery] = useState('');
+   const [search, setSearch] = useState('');
+   const [pass, setPass] = useState(false);
+
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      setPass(true);
+   }
+
+   if (pass) {
+      return (
+         <>
+            <Profile query={query} searchtype={search} />
+         </>);
+   }
+
+   return (<>
+      <div className='back'>
+         <div id='heading' className='block'>
+
+            <h1>Footy Pedia</h1>
+            <p>Your Wiki for football related stuff</p>
+
+            <div className='field' onSubmit={handleSubmit}>
+               <input
+                  type="text"
+                  name="query"
+                  id="query"
+                  placeholder="Enter Query"
+                  className='input control'
+                  value={query}
+                  onChange={(e) => {
+                     setQuery(e.target.value)
+                     setPass(false)
+                  }}
+                  required
+               />
+               <div className='select'>
+                  <select name="search_type" id="selection"
+                     onChange={(e) => { setSearch(e.target.value) }}>
+                     <option value="Category">Category</option>
+                     <option value="Player">Player</option>
+                     <option value="Team">Team</option>
+                  </select>
+               </div>
+            </div>
+            <button type='submit' className='button is-rounded' id='submit'
+               onClick={handleSubmit} >Submit</button>
+         </div>
+      </div>
+   </>);
+}
+
+export default Home
